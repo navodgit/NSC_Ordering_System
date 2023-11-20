3d0ff1ffa03df988ddd9d04c1063482f5588476a
navodgit <kalansooriyand.20@uom.lk>
2023-11-20 22:48:49 +0530

    Initial Commit

--- Restaurant/ROS/NSC/models.py
+++ Restaurant/ROS/NSC/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class NSC(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+
+class customer(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
