2d880ff9468785750e5ab0efa8189c4de2c6acc0
samidututorial <147871788+samidututorial@users.noreply.github.com>
2023-12-04 11:35:30 +0530

    Did some changes in apps.py file

--- Restaurant/ROS/hotel/apps.py
+++ Restaurant/ROS/hotel/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class NSCConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'NSC'
+class HotelConfig(AppConfig):
+    name = 'hotel'
